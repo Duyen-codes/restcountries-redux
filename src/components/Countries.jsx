23c42325dad4593c455b265fa5044d66efd98d70
Duyen Nguyen <hongduyen0705@gmail.com>
2022-10-05 11:16:14 +0300

    combined import react and useEffect

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeCountries } from "../reducers/countryReducer";
 import { setIsLoading } from "../reducers/loadingReducer";
